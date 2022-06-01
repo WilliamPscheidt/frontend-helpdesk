0a13ff343f2c5c3345e001f5c0a30c03aed3dfa1
William Pscheidt <williampscheidt@hotmail.com>
2022-06-01 13:42:17 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./Components/HomePage/HomePage";
+import InsertFaqs from "./Components/Administration/InsertFaqs/InsertFaqs";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/admin-login/" element={<HomePage />} />
+        <Route path="/admin/insertfaqs" element={<HomePage />} />
       </Routes>
     </Router>
   );
